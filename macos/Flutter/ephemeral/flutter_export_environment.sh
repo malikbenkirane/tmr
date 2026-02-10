4674715436cd7b822b5ce6bc795416eb12729949
Malik Benkirane <mbenkirane@internetworks.blog>
2026-02-10 22:10:19 +0100

    macos and windows support
    part of #58, #61

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/malikbenkirane/Downloads/flutter-develop/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/malikbenkirane/Downloads/flutter-develop/too_many_tabs"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9WRVJTSU9OPTMuNDEuMC0wLjAucHJl,RkxVVFRFUl9DSEFOTkVMPWJldGE=,RkxVVFRFUl9HSVRfVVJMPWdpdEBnaXRodWIuY29tOmZsdXR0ZXIvZmx1dHRlci5naXQ=,RkxVVFRFUl9GUkFNRVdPUktfUkVWSVNJT049ZWUzN2I5NzQwNQ==,RkxVVFRFUl9FTkdJTkVfUkVWSVNJT049YjI2NzA5MGUzMQ==,RkxVVFRFUl9EQVJUX1ZFUlNJT049My4xMS4wIChidWlsZCAzLjExLjAtMjk2LjMuYmV0YSk="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=true"
+export "PACKAGE_CONFIG=/Users/malikbenkirane/Downloads/flutter-develop/too_many_tabs/.dart_tool/package_config.json"
---
+++ macos/Pods/Target Support Files/clipboard/clipboard-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_clipboard : NSObject
+@end
+@implementation PodsDummy_clipboard
+@end
