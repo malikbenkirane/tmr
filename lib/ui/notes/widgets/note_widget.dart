67bec87d17eabad0406ca242e9314f640631a098
Malik Benkirane <mbenkirane@internetworks.blog>
2026-03-10 23:05:16 +0000

    test: verify tap region registers events
    Tests whether a TapRegion registers tap events correctly.
    
    Part of the investigation of #106

--- lib/ui/notes/widgets/note_widget.dart
+++ lib/ui/notes/widgets/note_widget.dart
@@ -39,19 +39,18 @@ class NoteWidget extends StatelessWidget {
     return Material(
       borderRadius: BorderRadius.circular(5),
       elevation: .2,
-      child: InkWell(
-        onTap: () {
+      child: TapRegion(
+        onTapInside: (_) {
           _launchInBrowser(context, text);
         },
-        borderRadius: BorderRadius.circular(5),
-
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 4),
+          padding: EdgeInsets.symmetric(vertical: 2, horizontal: 4),
           child: Text(
             text,
             style: TextStyle(
               color: labelColor(context, Label.noteLink),
               fontWeight: note.dismissed ? FontWeight.w400 : null,
+              fontSize: 12,
             ),
           ),
         ),
