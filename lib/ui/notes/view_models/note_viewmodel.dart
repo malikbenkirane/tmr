70b9b5891ee72c83ff3113b507330b2e36bf462b
Malik Benkirane <mbenkirane@internetworks.blog>
2026-03-11 00:12:44 +0000

    minimal note screen

--- lib/ui/notes/view_models/note_viewmodel.dart
+++ lib/ui/notes/view_models/note_viewmodel.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:too_many_tabs/data/repositories/routines/routines_repository.dart';
 import 'package:too_many_tabs/domain/models/notes/note_summary.dart';
+import 'package:too_many_tabs/domain/models/routines/routine_summary.dart';
 import 'package:too_many_tabs/utils/command.dart';
 import 'package:too_many_tabs/utils/result.dart';
 
@@ -17,6 +18,9 @@ class NoteViewmodel extends ChangeNotifier {
   NoteSummary? _note;
   NoteSummary? get note => _note;
 
+  RoutineSummary? _routine;
+  RoutineSummary? get routine => _routine;
+
   Future<Result<void>> _load() async {
     try {
       final result = await routinesRespository.getNote(noteId);
@@ -25,6 +29,17 @@ class NoteViewmodel extends ChangeNotifier {
           return Result.error(result.error);
         case Ok<NoteSummary>():
           _note = result.value;
+          {
+            final result = await routinesRespository.getRoutineSummary(
+              _note!.routineId,
+            );
+            switch (result) {
+              case Error<RoutineSummary>():
+                return Result.error(result.error);
+              case Ok<RoutineSummary>():
+                _routine = result.value;
+            }
+          }
       }
       return Result.ok(null);
     } finally {
--- lib/ui/notes/widgets/note_screen.dart
+++ lib/ui/notes/widgets/note_screen.dart
@@ -1,10 +1,12 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:material_symbols_icons/symbols.dart';
 import 'package:too_many_tabs/routing/routes.dart';
+import 'package:too_many_tabs/ui/core/loader.dart';
 import 'package:too_many_tabs/ui/core/ui/application_action.dart';
 import 'package:too_many_tabs/ui/notes/view_models/note_viewmodel.dart';
 import 'package:too_many_tabs/ui/core/ui/floating_action.dart';
+import 'package:too_many_tabs/ui/notes/widgets/note_widget.dart';
 
 class NoteScreen extends StatelessWidget {
   final NoteViewmodel viewModel;
@@ -17,20 +19,65 @@ class NoteScreen extends StatelessWidget {
       body: SafeArea(
         child: Stack(
           children: [
+            Padding(
+              padding: EdgeInsets.all(40),
+              child: ListenableBuilder(
+                listenable: viewModel.load,
+                builder: (context, child) {
+                  return Loader(
+                    error: viewModel.load.error,
+                    running: viewModel.load.running,
+                    onError: viewModel.load.execute,
+                    child: child!,
+                  );
+                },
+                child: ListenableBuilder(
+                  listenable: viewModel,
+                  builder: (context, _) {
+                    final note = viewModel.note;
+                    if (note == null) return SizedBox.shrink();
+                    return (NoteWidget(note: note));
+                  },
+                ),
+              ),
+            ),
             Align(
               alignment: Alignment.bottomCenter,
               child: Padding(
-                padding: EdgeInsetsGeometry.symmetric(horizontal: 20),
+                padding: EdgeInsets.symmetric(
+                  vertical: Platform.isIOS || Platform.isAndroid ? 0 : 20,
+                  horizontal: 30,
+                ),
                 child: Row(
                   mainAxisAlignment: MainAxisAlignment.end,
                   children: [
+                    Expanded(
+                      child: GestureDetector(
+                        onTap: () {
+                          final note = viewModel.note;
+                          if (note == null) return;
+                          context.go('${Routes.notes}/${note.routineId}');
+                        },
+                        child: ListenableBuilder(
+                          listenable: viewModel,
+                          builder: (context, _) {
+                            final routine = viewModel.routine;
+                            final note = viewModel.note;
+                            if (routine == null || note == null) {
+                              return SizedBox.shrink();
+                            }
+                            return Text(routine.name);
+                          },
+                        ),
+                      ),
+                    ),
                     FloatingAction(
                       onPressed: () => context.go(Routes.home),
                       colorComposition: colorCompositionFromAction(
                         context,
                         ApplicationAction.toHome,
                       ),
-                      icon: Icon(Symbols.home),
+                      icon: Icon(Icons.home),
                     ),
                   ],
                 ),
